7e981268e3692fa18cf3e15bd376479df964ac62
aakzsh <aakashferrari@gmail.com>
2024-03-29 19:24:42 +0530

    setup raspberry pi side

--- raspberry/app.py
+++ raspberry/app.py
@@ -3,8 +3,23 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def dashboard():
+    return 'show the nurse dashboard here'
+
+@app.route('/alert')
+def alert():
+    # send alert to nurse phones
+    return "alert!"
+
+@app.route('/edit')
+def edit():
+    # edit patient details from here
+    return "patient details edit"
+
+@app.route('/nlp')
+def edit():
+    # get prompt, generate and return response
+    return "response"
 
 if __name__ == '__main__':
     app.run()
---
+++ raspberry/nlp.py
@@ -0,0 +1,13 @@
+# this would contain the code for the nlp mode part
+
+
+# while true:
+#     keep listening for audio
+
+#     if audio doesnt contains "smart assistant":
+#         ignore
+#     else:
+#         get the audio, convert to text
+#         pass text query to nlp mode, get response
+#         POST request at /nlp
+#         return the response to patient as audio
